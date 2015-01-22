a738bdb4b483455cc47185f7fb581dd74523ffbf
Antoine Ponsard <aponsard@cs.ubc.ca>
2015-01-21 23:41:18 -0800

    First implementation of black and white effect
    for some reason, when applying the filter on web-page body, all the
    elements disappear...

--- main.js
+++ main.js
@@ -19,16 +19,24 @@ var hooks;
 
 if(customizationModeOn === undefined){
 	customizationModeOn=true;
-	$("body").append("<div id='overlay'></div>");
+	
+	// wrap the content of the webpage in a div
+	$("body").children().wrapAll("<div id='webpage-body'></div>");
+	$("#webpage-body").css(getAllCSS($("body")));
+	$(".main-interface, #wunderlist-base.background-01:before").addClass("dimmed");
 
-	// stores the current coordinates
+	// add some structure for the customization mode
+	$("body").append("<div id='overlay'></div>");
+	$("body").append("<div id='hooks'></div>");
+
+	// store the current coordinates
 	customizableElements.each(function(){
 		$(this).data("coordinates",$(this).offset());
 	})
 
 	// clone with their data, but remove event binders with .off()
 	hooks=customizableElements.clone(true).off();
-	hooks.appendTo("body");
+	hooks.appendTo("#hooks");
 
 	// position the hooks on top of the elements
 	hooks.each(function(){
@@ -44,7 +52,40 @@ if(customizationModeOn === undefined){
 }
 else{
 	customizationModeOn=undefined;
-	$("#overlay").remove();
+	$("#overlay, #hooks").remove();
 	hooks.remove();
-	//$(customizableElements.join()).removeClass("customizable");
+}
+
+
+
+function getAllCSS(a) {
+    var sheets = document.styleSheets, o = {};
+    for (var i in sheets) {
+        var rules = sheets[i].rules || sheets[i].cssRules;
+        for (var r in rules) {
+            if (a.is(rules[r].selectorText)) {
+                o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
+            }
+        }
+    }
+    return o;
+}
+
+function css2json(css) {
+    var s = {};
+    if (!css) return s;
+    if (css instanceof CSSStyleDeclaration) {
+        for (var i in css) {
+            if ((css[i]).toLowerCase) {
+                s[(css[i]).toLowerCase()] = (css[css[i]]);
+            }
+        }
+    } else if (typeof css == "string") {
+        css = css.split("; ");
+        for (var i in css) {
+            var l = css[i].split(": ");
+            s[l[0].toLowerCase()] = (l[1]);
+        }
+    }
+    return s;
 }
