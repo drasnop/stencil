28dc5b92c29b1198b11fe3ff2f18b4b2280fc35c
Antoine Ponsard <aponsard@cs.ubc.ca>
2015-01-28 11:04:23 -0800

    new organization of the options data

--- data.js
+++ data.js
@@ -68,71 +68,335 @@ var mapping = [
 	}
 ];
 var options={
-	"_locale": ["en"],
-	"account_locale": ["en_GB"],
-	"add_to_chrome": false,
-	"add_to_firefox": false,
-	"app_first_used": ["1421913600000"],
-	"auto_reminder_noticeperiod": ["0"],
-	"auto_reminder_timeinterval": ["540"],
-	"background": ["wlbackground15"],
-	"behavior_star_tasks_to_top": true,
-	"chrome_app_rating_later": ["undefined"],
-	"chrome_rating_later": ["undefined"],
-	"confirm_delete_entity": true,
-	"date_format": ["DD.MM.YYYY"],
-	"enable_natural_date_recognition": true,
-	"id": ["userSettings"],
-	"language": ["en_GB"],
-	"last_open_app_date": ["0"],
-	"migrated_wunderlist_one_user": true,
-	"new_installation": false,
-	"new_task_location": ["top"],
-	"newsletter_subscription_enabled": false,
-	"notifications_desktop_enabled": true,
-	"notifications_email_enabled": true,
-	"notifications_push_enabled": true,
-	"onboarding_add_todo": true,
-	"onboarding_click_create_list": true,
-	"onboarding_click_share_list": true,
-	"print_completed_items": false,
-	"pro_trial_limit_assigning": ["3"],
-	"pro_trial_limit_comments": ["10"],
-	"pro_trial_limit_files": ["3"],
-	"shortcut_add_new_list": ["CTRL + L"],
-	"shortcut_add_new_task": ["CTRL + 0"],
-	"shortcut_copy_tasks": ["CTRL + C"],
-	"shortcut_cut_tasks": ["CTRL + X"],
-	"shortcut_delete": ["CTRL + BACKSPACE"],
-	"shortcut_goto_filter_all": ["CTRL + 5"],
-	"shortcut_goto_filter_assigned": ["CTRL + 1"],
-	"shortcut_goto_filter_completed": ["CTRL + 6"],
-	"shortcut_goto_filter_starred": ["CTRL + 2"],
-	"shortcut_goto_filter_today": ["CTRL + 3"],
-	"shortcut_goto_filter_week": ["CTRL + 4"],
-	"shortcut_goto_inbox": ["CTRL + I"],
-	"shortcut_goto_preferences": ["CTRL + P"],
-	"shortcut_goto_search": ["CTRL + F"],
-	"shortcut_mark_task_done": ["CTRL + D"],
-	"shortcut_mark_task_starred": ["CTRL + S"],
-	"shortcut_paste_tasks": ["CTRL + V"],
-	"shortcut_select_all_tasks": ["CTRL + A"],
-	"shortcut_send_via_email": ["CTRL + E"],
-	"shortcut_show_notifications": ["CTRL + SHIFT + A"],
-	"shortcut_sync": ["R"],
-	"show_completed_items": true,
-	"significant_event_count": ["0"],
-	"smartlist_visibility_all": ["auto","visible","hidden"],
-	"smartlist_visibility_assigned_to_me": ["auto","visible","hidden"],
-	"smartlist_visibility_done": ["auto","visible","hidden"],
-	"smartlist_visibility_starred": ["auto","visible","hidden"],
-	"smartlist_visibility_today": ["auto","visible","hidden"],
-	"smartlist_visibility_week": ["auto","visible","hidden"],
-	"sound_checkoff_enabled": true,
-	"sound_notification_enabled": true,
-	"start_of_week": ["sun"],
-	"time_format": ["12 hour"],
-	"today_smart_list_visible_tasks": ["all"],
-	"type": ["userSettings"]
+	"_locale": {
+		"label": "",
+		"value": "en",
+		"values": ["en"]
+	},
+	"account_locale": {
+		"label": "",
+		"value": "en_GB",
+		"values": ["en_GB"]
+	},
+	"add_to_chrome": {
+		"label": "",
+		"value": false,
+		"values": []
+	},
+	"add_to_firefox": {
+		"label": "",
+		"value": false,
+		"values": []
+	},
+	"app_first_used": {
+		"label": "",
+		"value": 1421913600000,
+		"values": ["1421913600000"]
+	},
+	"auto_reminder_noticeperiod": {
+		"label": "",
+		"value": 0,
+		"values": ["0"]
+	},
+	"auto_reminder_timeinterval": {
+		"label": "",
+		"value": 540,
+		"values": ["540"]
+	},
+	"background": {
+		"label": "",
+		"value": "wlbackground15",
+		"values": ["wlbackground15"]
+	},
+	"behavior_star_tasks_to_top": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"chrome_app_rating_later": {
+		"label": "",
+		"value": "undefined",
+		"values": ["undefined"]
+	},
+	"chrome_rating_later": {
+		"label": "",
+		"value": "undefined",
+		"values": ["undefined"]
+	},
+	"confirm_delete_entity": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"date_format": {
+		"label": "",
+		"value": "DD.MM.YYYY",
+		"values": ["DD.MM.YYYY"]
+	},
+	"enable_natural_date_recognition": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"id": {
+		"label": "",
+		"value": "userSettings",
+		"values": ["userSettings"]
+	},
+	"language": {
+		"label": "",
+		"value": "en_GB",
+		"values": ["en_GB"]
+	},
+	"last_open_app_date": {
+		"label": "",
+		"value": 0,
+		"values": ["0"]
+	},
+	"migrated_wunderlist_one_user": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"new_installation": {
+		"label": "",
+		"value": false,
+		"values": []
+	},
+	"new_task_location": {
+		"label": "",
+		"value": "top",
+		"values": ["top"]
+	},
+	"newsletter_subscription_enabled": {
+		"label": "",
+		"value": false,
+		"values": []
+	},
+	"notifications_desktop_enabled": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"notifications_email_enabled": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"notifications_push_enabled": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"onboarding_add_todo": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"onboarding_click_create_list": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"onboarding_click_share_list": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"print_completed_items": {
+		"label": "",
+		"value": false,
+		"values": []
+	},
+	"pro_trial_limit_assigning": {
+		"label": "",
+		"value": 3,
+		"values": ["3"]
+	},
+	"pro_trial_limit_comments": {
+		"label": "",
+		"value": 10,
+		"values": ["10"]
+	},
+	"pro_trial_limit_files": {
+		"label": "",
+		"value": 3,
+		"values": ["3"]
+	},
+	"shortcut_add_new_list": {
+		"label": "",
+		"value": "CTRL + L",
+		"values": ["CTRL + L"]
+	},
+	"shortcut_add_new_task": {
+		"label": "",
+		"value": "CTRL + 0",
+		"values": ["CTRL + 0"]
+	},
+	"shortcut_copy_tasks": {
+		"label": "",
+		"value": "CTRL + C",
+		"values": ["CTRL + C"]
+	},
+	"shortcut_cut_tasks": {
+		"label": "",
+		"value": "CTRL + X",
+		"values": ["CTRL + X"]
+	},
+	"shortcut_delete": {
+		"label": "",
+		"value": "CTRL + BACKSPACE",
+		"values": ["CTRL + BACKSPACE"]
+	},
+	"shortcut_goto_filter_all": {
+		"label": "",
+		"value": "CTRL + 5",
+		"values": ["CTRL + 5"]
+	},
+	"shortcut_goto_filter_assigned": {
+		"label": "",
+		"value": "CTRL + 1",
+		"values": ["CTRL + 1"]
+	},
+	"shortcut_goto_filter_completed": {
+		"label": "",
+		"value": "CTRL + 6",
+		"values": ["CTRL + 6"]
+	},
+	"shortcut_goto_filter_starred": {
+		"label": "",
+		"value": "CTRL + 2",
+		"values": ["CTRL + 2"]
+	},
+	"shortcut_goto_filter_today": {
+		"label": "",
+		"value": "CTRL + 3",
+		"values": ["CTRL + 3"]
+	},
+	"shortcut_goto_filter_week": {
+		"label": "",
+		"value": "CTRL + 4",
+		"values": ["CTRL + 4"]
+	},
+	"shortcut_goto_inbox": {
+		"label": "",
+		"value": "CTRL + I",
+		"values": ["CTRL + I"]
+	},
+	"shortcut_goto_preferences": {
+		"label": "",
+		"value": "CTRL + P",
+		"values": ["CTRL + P"]
+	},
+	"shortcut_goto_search": {
+		"label": "",
+		"value": "CTRL + F",
+		"values": ["CTRL + F"]
+	},
+	"shortcut_mark_task_done": {
+		"label": "",
+		"value": "CTRL + D",
+		"values": ["CTRL + D"]
+	},
+	"shortcut_mark_task_starred": {
+		"label": "",
+		"value": "CTRL + S",
+		"values": ["CTRL + S"]
+	},
+	"shortcut_paste_tasks": {
+		"label": "",
+		"value": "CTRL + V",
+		"values": ["CTRL + V"]
+	},
+	"shortcut_select_all_tasks": {
+		"label": "",
+		"value": "CTRL + A",
+		"values": ["CTRL + A"]
+	},
+	"shortcut_send_via_email": {
+		"label": "",
+		"value": "CTRL + E",
+		"values": ["CTRL + E"]
+	},
+	"shortcut_show_notifications": {
+		"label": "",
+		"value": "CTRL + SHIFT + A",
+		"values": ["CTRL + SHIFT + A"]
+	},
+	"shortcut_sync": {
+		"label": "",
+		"value": "R",
+		"values": ["R"]
+	},
+	"show_completed_items": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"significant_event_count": {
+		"label": "",
+		"value": 0,
+		"values": ["0"]
+	},
+	"smartlist_visibility_all": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"smartlist_visibility_assigned_to_me": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"smartlist_visibility_done": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"smartlist_visibility_starred": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"smartlist_visibility_today": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"smartlist_visibility_week": {
+		"label": "",
+		"value": "visible",
+		"values": ["auto","visible","hidden"]
+	},
+	"sound_checkoff_enabled": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"sound_notification_enabled": {
+		"label": "",
+		"value": true,
+		"values": []
+	},
+	"start_of_week": {
+		"label": "",
+		"value": "sun",
+		"values": ["sun"]
+	},
+	"time_format": {
+		"label": "",
+		"value": "12 hour",
+		"values": ["12 hour"]
+	},
+	"today_smart_list_visible_tasks": {
+		"label": "",
+		"value": "all",
+		"values": ["all"]
+	},
+	"type": {
+		"label": "",
+		"value": "userSettings",
+		"values": ["userSettings"]
+	}
 };
 var KEYCODE_ESC = 27;
