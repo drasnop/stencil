afc04355acbad282044013797d6abe8fcef9ef2d
antoine <ponsard.antoine@gmail.com>
2015-03-25 15:18:23 -0700

    Allow storing mapping in a variable (then use copy())

--- scripts/matchmaker.js
+++ scripts/matchmaker.js
@@ -3,21 +3,42 @@ $('body').append("<div id='selector-panel'></div>")
 $("#selector-panel").text("waiting for selector")
 
 selector = '';
-$('body').off().mousemove(function (e) {
+mapping = [];
+
+// remove all events binding, to operate on a static page
+$("*").off();
+
+$('body').mousemove(function (e) {
    // to avoid unnecessary overhead
    if (getSelector(e.target) != selector) {
 
-   // de-highlight previous elements
-   $(selector).removeClass("highlighted");
+      // de-highlight previous elements
+      $(selector).removeClass("highlighted");
 
-   selector=getSelector(e.target)
-   $("#selector-panel").text(selector+" ("+$(selector).length+")");
 
-   // highlight all similar elements
-   console.log($(selector).length)
-   $(selector).addClass("highlighted")
- }
+      selector=getSelector(e.target)
+      $("#selector-panel").text(selector+" ("+$(selector).length+")");
+
+      // highlight all similar elements
+      $(selector).addClass("highlighted")
+   }
 });
+
+/*
+   other exploration functions:
+   decrease specificity= remove the last class in the list
+   select parent (with same specificity)
+*/
+
+$("body").click(function(){
+   if(selector==="")
+      return;
+   console.log("saving: ",selector)
+   mapping.push({
+      "selector":selector,
+      "options":selector /* temporary fix */
+   })
+})
 
 function getSelector(elm, depth){
    if(!elm.className)
