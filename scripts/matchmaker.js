3a59d82803e6b6b0312cd9c2696b4f21e2c8511f
antoine <ponsard.antoine@gmail.com>
2015-03-26 13:54:34 -0700

    fixed bug in formatting classNames in selector

--- scripts/matchmaker.js
+++ scripts/matchmaker.js
@@ -4,76 +4,78 @@ $("#selector-panel").text("waiting for selector")
 
 selector = '';
 target = false;
-mapping = [];
+mappings = [];
 
 // remove all events binding, to operate on a static page
 $("*").off();
 
-$('body').mousemove(function (e) {
-   target=e.target;
+$('body').mousemove(function(e) {
+   target = e.target;
    updateView(getSelector(target));
 });
 
 
-$("body").click(function(){
-   if(selector==="")
+$("body").click(function() {
+   if(selector === "")
       return;
-   console.log("saving: ",selector)
-   mapping.push({
-      "selector":selector,
-      "options":selector /* temporary fix */
+   console.log("saving: ", selector)
+   mappings.push({
+      "selector": selector,
+      "options": selector /* temporary fix */
    })
 })
 
-$("body").keypress(function(e){
+$("body").keypress(function(e) {
    console.log(e.which);
-   
+
    // d: decresase specificity
    var newSelector, arr;
-   if(e.which==100){
-      arr=selector.split(".")
-      if(arr.length>2)
+   if(e.which == 100) {
+      arr = selector.split(".")
+      if(arr.length > 2)
          arr.pop();
-      newSelector=arr.join(".");
+      newSelector = arr.join(".");
 
       console.log("decrease specificity")
       updateView(newSelector);
    }
 
    // e: expand to parent
-   if(e.which==101){
-      newSelector=target.parentNode.tagName.toLowerCase() + getSelector(target.parentNode);
-      console.log("expand to parent node: "+target.parentNode.tagName.toLowerCase())
+   if(e.which == 101) {
+      newSelector = target.parentNode.tagName.toLowerCase() + getSelector(target.parentNode);
+      console.log("expand to parent node: " + target.parentNode.tagName.toLowerCase())
       updateView(newSelector);
    }
 })
 
 
-function getSelector(elm){
+function getSelector(elm) {
    if(!elm.className)
-   return "";
+      return "";
 
-   var s="."+elm.className, arr;
-   if(s[s.length-1]==" ")
-      s=s.slice(0, s.length-1);
+   var s = elm.className;
 
-   arr=s.split(" ");
-   if(arr[arr.length-1]=="highlighted")
-      arr.pop();
+   // cleanup to remove any additional whitespace and my own added class
+   var arr = s.split(" ");
+   arr = arr.filter(function(n) {
+      return n !== "" && n !== "highlighted";
+   })
 
-   s=arr.join(".");
+   // create CSS classes
+   s = "." + arr.join(".");
+
    return s;
 }
 
-function updateView(newSelector){
+function updateView(newSelector) {
    // to avoid unnecessary overhead, update only if the selector has changed
-   if (newSelector != selector) {
+   if(newSelector != selector) {
 
       // de-highlight previous elements
       $(selector).removeClass("highlighted");
 
-      selector=newSelector;
-      $("#selector-panel").text(selector+" ("+$(selector).length+")");
+      selector = newSelector;
+      $("#selector-panel").text(selector + " (" + $(selector).length + ")");
 
       // highlight all similar elements
       $(selector).addClass("highlighted")
