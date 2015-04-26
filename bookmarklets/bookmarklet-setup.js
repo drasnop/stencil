2aa1cee887acf5d11a8ffda57165a544c0b31b24
drasnop <ponsard.antoine@gmail.com>
2015-04-26 13:02:01 -0700

    Python 3 ssl server + reformat bookmark

--- bookmarklets/bookmarklet-setup.js
+++ bookmarklets/bookmarklet-setup.js
@@ -1,25 +1,22 @@
 javascript:(function(e,a,g,h,f,c,b,d){
 	if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){
-		console.log("fetching jQuery "+g);
-		c=a.createElement("script");
-		c.type="text/javascript";
-		c.src="https://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";
+		console.log('fetching jQuery '+g);
+		c=a.createElement('script');
+		c.type='text/javascript';
+		c.src='https://ajax.googleapis.com/ajax/libs/jquery/'+g+'/jquery.min.js';
 		c.onload=c.onreadystatechange=function(){
-			if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){
+			if(!b&&(!(d=this.readyState)||d=='loaded'||d=='complete')){
 				h((f=e.jQuery).noConflict(1),b=1);
 				f(c).remove()
 			}
 		};
 		a.documentElement.childNodes[0].appendChild(c);
 	}else{
-		console.log("jQuery ",g,"already present")
+		console.log('jQuery ',g,'already present')
 	}
-})(window,document,"2.1.0",function(j,L){
+})(window,document,'2.1.0',function(j,L){
 	window.jQuery=j;
 	window.$=window.jQuery;
-
-	if ($("link[href='//localhost:8888/css/style.css']").length>0)
-		$("link[href='//localhost:8888/css/style.css']").remove();
 
 	$('<link/>', {
 		rel: 'stylesheet',
@@ -29,28 +26,28 @@ javascript:(function(e,a,g,h,f,c,b,d){
 	}).appendTo('head');
 
 	$.when(
-		$.getScript("//ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.js"),
+		$.getScript('//ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.js'),
 		$.Deferred(function( deferred ){
 			$( deferred.resolve );
 		})
 	).done(function(){
 		$.when(
-			$.getScript("//ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular-animate.js"),
+			$.getScript('//ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular-animate.js'),
 			$.Deferred(function( deferred ){
 				$( deferred.resolve );
 			})
 		).done(function(){
 			$.when(
-				$.getScript("//localhost:8888/libs/jquery-ui-position.js"),
-				$.getScript("//localhost:8888/js/global.js"),
-				$.getScript("//localhost:8888/js/geometry.js"),
-				$.getScript("//localhost:8888/js/dataManager.js"),
-				$.getScript("//localhost:8888/js/app.js"),
-				$.getScript("//localhost:8888/js/controller.js"),
-				$.getScript("//localhost:8888/js/listeners.js"),
-				$.getScript("//localhost:8888/js/hooks.js"),
-				$.getScript("//localhost:8888/js/helpers.js"),
-				$.getScript("//localhost:8888/js/main.js"),
+				$.getScript('//localhost:8888/libs/jquery-ui-position.js'),
+				$.getScript('//localhost:8888/js/global.js'),
+				$.getScript('//localhost:8888/js/geometry.js'),
+				$.getScript('//localhost:8888/js/dataManager.js'),
+				$.getScript('//localhost:8888/js/app.js'),
+				$.getScript('//localhost:8888/js/controller.js'),
+				$.getScript('//localhost:8888/js/listeners.js'),
+				$.getScript('//localhost:8888/js/hooks.js'),
+				$.getScript('//localhost:8888/js/helpers.js'),
+				$.getScript('//localhost:8888/js/main.js'),
 				$.Deferred(function( deferred ){
 					$( deferred.resolve );
 				})
