72a8767c8dd8ca180fb2667f0b8f8cd00a881be4
drasnop <ponsard.antoine@gmail.com>
2015-05-11 21:48:44 -0700

    Basic results parser

---
+++ scripts/results-parser.js
@@ -0,0 +1,27 @@
+var filepath = 'results/incandescent-torch-4042-mturk-export.json';
+
+Object.defineProperty(Object.prototype, "forEach", {
+   value: function(callback) {
+      Object.keys(this).forEach(function(key) {
+         callback(this[key]);
+      }, this)
+   }
+})
+
+var fs = require('fs');
+
+var mturk = JSON.parse(fs.readFileSync(filepath, 'utf8'));
+console.log("Resuls file parsed: ", filepath, mturk)
+
+var stream = fs.createWriteStream("results.csv");
+stream.once('open', function(fd) {
+   stream.write("My first row\n");
+   stream.write("My second row\n");
+   stream.end();
+});
+
+mturk.forEach(function(user) {
+   user.trials.forEach(function(trial) {
+      console.log(trial.number)
+   })
+})
