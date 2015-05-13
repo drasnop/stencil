9d140a7b39f16a574223b31de9f21a226f6ce983
drasnop <ponsard.antoine@gmail.com>
2015-05-13 11:29:07 -0700

    Bigger select boxes, to look better and more like Wunderlists
    need to set a fixed height of 38 px

--- js/geometry.js
+++ js/geometry.js
@@ -4,22 +4,22 @@ var geometry = {
 }
 
 geometry.getOptionHeight = function() {
-   if(model.optionsVisibility == 1 && !model.fullPanel())
+   if (model.optionsVisibility == 1 && !model.fullPanel())
       return 38;
 
-   return 28;
+   return 38;
 }
 
 geometry.getOptionTop = function(filteredIndex) {
    var top = filteredIndex * geometry.getOptionHeight();
-   if(model.fullPanel() && typeof model.activeTab.description !== "undefined")
+   if (model.fullPanel() && typeof model.activeTab.description !== "undefined")
       top += 78;
    return top;
 }
 
 geometry.getTotalHeight = function(numberVisibleOptions) {
    var totalHeight = numberVisibleOptions * geometry.getOptionHeight();
-   if(model.fullPanel() && typeof model.activeTab.description !== "undefined")
+   if (model.fullPanel() && typeof model.activeTab.description !== "undefined")
       totalHeight += 78;
    return totalHeight;
 }
