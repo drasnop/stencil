d765cc94b3f332b00ad88688d8016d8741c786a4
drasnop <ponsard.antoine@gmail.com>
2015-06-23 19:34:20 -0700

    refactoring: closure around geometry

--- js/geometry.js
+++ js/geometry.js
@@ -1,41 +1,51 @@
-var geometry = {
-   "panelHeight": 373,
-   "tabsHeight": 45,
-   "optionHeight": 38
-}
-
-geometry.getOptionHeight = function() {
-   // thus, all options have the same height (checkboxes are simply vertically centered)
-   return geometry.optionHeight;
-}
-
-geometry.getOptionTop = function(filteredIndex) {
-   return computeHeightIncludingDescription(filteredIndex);
-}
-
-// return a string to be used in inline css
-geometry.getAllOptionsHeight = function() {
-   var numVisibleOptions = angular.element($("#ad-hoc-panel")).scope().getTotalNumberVisibleOptions();
-
-   if (model.fullPanel() && model.activeTab.bloat)
-      return "auto";
-   else
-      return computeHeightIncludingDescription(numVisibleOptions);
-}
-
-geometry.getPanelHeight = function() {
-   if (model.fullPanel()) {
-      // thus far, a fixed height
-      return geometry.panelHeight;
-   } else {
-      // take into account the margins top and bottom of #options-list
-      return geometry.getAllOptionsHeight() + 8 + 20;
+var geometry = (function() {
+   var geometry = {
+      "panelHeight": 373,
+      "tabsHeight": 45,
+      "optionHeight": 38
    }
-}
 
-function computeHeightIncludingDescription(numOptions) {
-   var result = numOptions * geometry.getOptionHeight();
-   if (model.fullPanel() && model.activeTab.description)
-      result += 58;
-   return result;
-}
+   geometry.getOptionHeight = function() {
+      // thus, all options have the same height (checkboxes are simply vertically centered)
+      return geometry.optionHeight;
+   }
+
+   geometry.getOptionTop = function(filteredIndex) {
+      return computeHeightIncludingDescription(filteredIndex);
+   }
+
+   // return a string to be used in inline css
+   geometry.getAllOptionsHeight = function() {
+      var numVisibleOptions = angular.element($("#ad-hoc-panel")).scope().getTotalNumberVisibleOptions();
+
+      if (model.fullPanel() && model.activeTab.bloat)
+         return "auto";
+      else
+         return computeHeightIncludingDescription(numVisibleOptions);
+   }
+
+   geometry.getPanelHeight = function() {
+      if (model.fullPanel()) {
+         // thus far, a fixed height
+         return geometry.panelHeight;
+      } else {
+         // take into account the margins top and bottom of #options-list
+         return geometry.getAllOptionsHeight() + 8 + 20;
+      }
+   }
+
+   geometry.getPanelWidth = function() {
+      var maxWidth = 0
+
+      return maxWidth + (model.optionsVisibility == 2) ? geometry.linksToTabWidth : 0;
+   }
+
+   function computeHeightIncludingDescription(numOptions) {
+      var result = numOptions * geometry.getOptionHeight();
+      if (model.fullPanel() && model.activeTab.description)
+         result += 58;
+      return result;
+   }
+
+   return geometry;
+})();
