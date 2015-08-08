6d59899b60ef26e2cbd2f9122c4aa3e47a4ac6e8
drasnop <ponsard.antoine@gmail.com>
2015-08-07 23:08:04 -0700

    Massive BFS to find the settings

--- scripts/scrapers.js
+++ scripts/scrapers.js
@@ -33,6 +33,61 @@ for (var c in sync.collections) {
       console.log(c, sync.collections[c].models.length)
 }
 
+function BFS(start) {
+   var queue = [start];
+   var obj;
+   var count = 0;
+   shortcuts = [];
+
+   shortcuts._visited = true;
+   queue[0]._visited = true;
+   while (queue.length) {
+      obj = queue.shift();
+
+      Object.keys(obj).forEach(function(key) {
+         if (obj[key] != null) {
+
+            if (typeof obj[key] == "object" && !obj[key]._visited) {
+               obj[key]._visited = true;
+               obj[key]._parent = obj;
+               obj[key]._parentCalledMe = key;
+               queue.push(obj[key]);
+
+            } else if (typeof obj[key] == "string") {
+               if (obj[key].indexOf("CTRL +") >= 0) {
+                  shortcuts.push(obj)
+               }
+               //console.log(obj[key])
+            }
+         }
+      });
+      // move on to next object in the queue
+      count++;
+   }
+   console.log("BFS completed after visiting " + count + " nodes");
+
+   console.log(shortcuts);
+
+   for (var i = 0; i < shortcuts.length; i++) {
+      for (var j = i + 1; j < shortcuts.length; j++) {
+         console.log(shortcuts[i] == shortcuts[j])
+      }
+   }
+
+   shortcuts.forEach(function(shortcut) {
+      var max = 50;
+      var obj = shortcut;
+      var count = 0;
+      var path = [];
+      while (obj != window && count < max) {
+         path.unshift(obj._parentCalledMe);
+         obj = obj._parent;
+         count++;
+      }
+      console.log(count, path);
+   })
+}
+
 var regex = new RegExp("_", 'g');
 for (var name in options) {
    var id = name.replace(regex, '-');
